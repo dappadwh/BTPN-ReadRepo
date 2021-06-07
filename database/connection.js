efb09d659f50f5aa00d6b42174fcdd76280a202c
rafi.daffa <raffidaffa67@gmail.com>
2021-06-07 12:21:34 +0700

    edit not using env variable

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 
 async function Collection(collection){
-    const url = process.env.MONGO_URL;
+    const url = "mongodb+srv://rafidaffa:P4ssword@dappadwh.zydll.mongodb.net/test"
     try {
           const db = await MongoClient.connect(url, {useUnifiedTopology: true});
           if (db) {
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 
+const JWT_SECRET_KEY = 'rahasia'
 function generateToken (data){
-    const token = jwt.sign(data, process.env.JWT_SECRET_KEY);
+    const token = jwt.sign(data, JWT_SECRET_KEY);
     return token;
 }
 
 function verifyToken(token) {
-    return jwt.verify(token, process.env.JWT_SECRET_KEY);
+    return jwt.verify(token, JWT_SECRET_KEY);
 }
 
 module.exports = {
